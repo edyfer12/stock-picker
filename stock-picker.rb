b3046ba6b11474c354c37ab0637c0cfbcfc2cc17
Edin <edyfer2@live.com.au>
2024-04-02 18:21:33 +0200

    Add comments

---
+++ stock-picker.rb
@@ -0,0 +1,16 @@
+#Declare a method that aims to select the best stock price to buy and sell and return the 
+#days in pairs in array form
+    #Declare an array aimed to store the days
+    #Declare a counter variable and set to 0
+    #Iterate through the array
+        #Between current counter and last index,
+            #Find the minimum value of the array
+            #Find the maximum value of the array
+            #Find the index of minimum value
+            #Find the index of maximum value
+            #If the index of maximum value is greater than index of minimum value,
+                #Return the array pair of index values of minimum and maximum value
+            #Otherwise, 
+                #Increment counter by 1
+
+#Invoke the stock-picker method that returns the best day to buy and sell
