2e3b58800999a0412de70908f3a1fad1c93e4581
Edin <edyfer2@live.com.au>
2024-04-02 23:09:49 +0200

    Simplify code

--- stock-picker.rb
+++ stock-picker.rb
@@ -11,14 +11,10 @@ def stock_picker(input)
     #Iterate through the array
     original_array.each do |price| 
         #Between current counter and last index,
-            #Find the minimum value of the array
-            buy = new_array.min()
-            #Find the maximum value of the array
-            sell = new_array.max()
             #Find the index of minimum value
-            buyIndex = original_array.index(buy)
+            buyIndex = original_array.index(new_array.min())
             #Find the index of maximum value
-            sellIndex = original_array.index(sell)
+            sellIndex = original_array.index(new_array.max())
             #If the index of maximum value is greater than index of minimum value,
             if buyIndex < sellIndex
                 #Return the array pair of index values of minimum and maximum value
@@ -35,4 +31,4 @@ end
 
 #Invoke the stock-picker method that returns the best day to buy and sell
 puts stock_picker([17,3,6,9,15,8,6,1,10])
-puts stock_picker([1,2,3,4,5])
+puts stock_picker([10,2,1,4,50,23])
